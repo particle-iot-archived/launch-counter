9776731869db9831167a3c6d0fb9435cdbf9c781
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-10-29 13:21:23 -0700

    Abstracted getCount and incrCount methods into separate functions

--- routes/orders.js
+++ routes/orders.js
@@ -5,42 +5,44 @@ var database = require('../lib/mongo.js');
 
 /* Get data about the orders. */
 router.get('/', function(req, res) {
-  database.query("counts", {name: "photons"}, function(err, arr) {
-    var photonCount = 0;
-    var orderCount = 0;
-    if (arr && (arr.length > 0)) {
-      var photons = arr[0];
-      photonCount = photons.count;
-    }
-    database.query("counts", {name: "orders"}, function(err, arr) {
-      if (arr && (arr.length > 0)) {
-        var orders = arr[0];
-        orderCount = orders.count;
-      }
+  getCount("photons", function(photonCount) {
+    getCount("orders", function(orderCount) {
       res.send({photons: photonCount, order: orderCount});
     });
-  });
+  })
 });
 
 router.post('/', function(req, res) {
   if (!req.body) return res.sendStatus(400);
   if (!req.body.photons && req.body.photons !== 0) return res.sendStatus(400);
 
-  database.findAndModify("counts", {name: "photons"}, {count: req.body.photons}, function(err, photons) {
+  incrCount("photons", req.body.photons, function(photons) {
+    incrCount("orders", 1, function(orders) {
+      res.send({photons: photons.count, order: orders.count});
+    });
+  });
+});
+
+function getCount(record, callback) {
+ database.query("counts", { name: record }, function(err, arr) {
+  var hasData = (arr && (arr.length > 0));
+  var count = (hasData && arr[0].count) ? arr[0].count : 0;
+  if (callback) { callback(count); }
+ });
+}
+
+
+function incrCount(record, val, callback) {
+  var filter = {name: record};
+  database.findAndModify("counts", filter, {count: val}, function(err, obj) {
       if (err){
           console.warn(err.message);  // returns error if no matching object found
       } else {
-        database.findAndModify("counts", {name: "orders"}, {count: 1}, function(err, orders) {
-          if (err){
-            console.warn(err.message);  // returns error if no matching object found
-          } else {
-            res.send({photons: photons.count, order: orders.count});
+          if (callback) {
+            callback(obj);
           }
-        });
       }
   });
-
-});
-
+}
 
 module.exports = router;
