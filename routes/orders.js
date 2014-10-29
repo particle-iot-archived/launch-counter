2b56e69a7e6f5edee9f4e84bb10e1cbf7dcf2a7f
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-10-29 12:59:44 -0700

    Added MongoDB support for the order and photons count

--- routes/orders.js
+++ routes/orders.js
@@ -2,25 +2,22 @@ var express = require('express');
 var router = express.Router();
 var database = require('../lib/mongo.js');
 
-var orders = 0;
-var photons = 0;
 
 /* Get data about the orders. */
 router.get('/', function(req, res) {
-  database.query("counts", { name: "photons" }, function(err, arr) {
+  database.query("counts", {name: "photons"}, function(err, arr) {
     var photonCount = 0;
     var orderCount = 0;
-
     if (arr && (arr.length > 0)) {
-      var doc = arr[0];
-      photonCount = doc.count;
+      var photons = arr[0];
+      photonCount = photons.count;
     }
-    database.query("counts", { name: "orders"}, function(err, arr) {
+    database.query("counts", {name: "orders"}, function(err, arr) {
       if (arr && (arr.length > 0)) {
-        var doc = arr[0];
-        orderCount = doc.count;
+        var orders = arr[0];
+        orderCount = orders.count;
       }
-      res.send({orders: orderCount, photons: photonCount});
+      res.send({photons: photonCount, order: orderCount});
     });
   });
 });
@@ -28,9 +25,22 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
   if (!req.body) return res.sendStatus(400);
   if (!req.body.photons && req.body.photons !== 0) return res.sendStatus(400);
-  orders++;
-  photons = photons + req.body.photons;
-  res.send({orders: orders, photons: photons});
+
+  database.findAndModify("counts", {name: "photons"}, {count: req.body.photons}, function(err, photons) {
+      if (err){
+          console.warn(err.message);  // returns error if no matching object found
+      } else {
+        database.findAndModify("counts", {name: "orders"}, {count: 1}, function(err, orders) {
+          if (err){
+            console.warn(err.message);  // returns error if no matching object found
+          } else {
+            res.send({photons: photons.count, order: orders.count});
+          }
+        });
+      }
+  });
+
 });
+
 
 module.exports = router;
