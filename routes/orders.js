26cbcc596c2ae815d095a4aeebf1f11e4149ebc1
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-10-28 18:42:34 -0700

    Added DB file

--- routes/orders.js
+++ routes/orders.js
@@ -1,12 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var database = require('../lib/mongo.js');
 
 var orders = 0;
 var photons = 0;
 
 /* Get data about the orders. */
 router.get('/', function(req, res) {
-  res.send({orders: orders, photons: photons});
+  database.query("counts", { name: "photons" }, function(err, arr) {
+    var photonCount = 0;
+    var orderCount = 0;
+
+    if (arr && (arr.length > 0)) {
+      var doc = arr[0];
+      photonCount = doc.count;
+    }
+    database.query("counts", { name: "orders"}, function(err, arr) {
+      if (arr && (arr.length > 0)) {
+        var doc = arr[0];
+        orderCount = doc.count;
+      }
+      res.send({orders: orderCount, photons: photonCount});
+    });
+  });
 });
 
 router.post('/', function(req, res) {
