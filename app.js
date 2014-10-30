8b77a9037c52717fffebfb941e8df0e296795ce9
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-10-29 17:46:54 -0700

    Added CORS headers

--- app.js
+++ app.js
@@ -58,6 +58,13 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  return next();
+});
+
 
 var debug = require('debug')('launch-counter');
 
