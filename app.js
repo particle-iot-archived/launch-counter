8613121b24a945d115504b68b5151e92d724a9e7
Jeff Eiden <jeff.m.eiden@gmail.com>
2014-10-29 18:32:10 -0700

    fixed cors

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function(err, req, res, next) {
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   return next();
 });
